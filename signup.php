0b9fa189a457b978da6d8a25701f559affe96128
Kejukalariya <kejukalariya5767@gmail.com>
2025-03-29 10:36:12 +0000

    changes

--- signup.php
+++ signup.php
@@ -112,7 +112,7 @@
                 
                 // Redirect to login page after a short delay
                 setTimeout(function() {
-                    window.location.href = 'login.php';
+                    window.location.href = 'login.html';
                 }, 1000); // 1 second delay
             }
         });
