14660cf1b95f4c2cedc765e5cd07cedc4f38b6c4
Kejukalariya <kejukalariya5767@gmail.com>
2025-03-29 10:16:18 +0000

    comment

--- signup.php
+++ signup.php
@@ -101,7 +101,7 @@
         </div>
     </div>
 
-    <script>
+    <!-- <script>
         document.getElementById('signupForm').addEventListener('submit', function(e) {
             e.preventDefault(); // Prevent default form submission
             
@@ -116,7 +116,7 @@
                 }, 1000); // 1 second delay
             }
         });
-        </script>
+        </script> -->
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
